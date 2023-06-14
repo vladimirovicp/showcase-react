f13f7490b6d159fa02da4fc0235ce638eb82fe37
vladimirovicp <vladimirovicp70@gmail.com>
2023-06-14 16:56:56 +0400

    005 Состояние заказа, иконка корзины

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,12 +3,14 @@ import {API_KEY, API_URL} from "../config";
 
 import {Preloader} from "./Preloader";
 import {GoodsList} from "./GoodsList";
+import {Cart} from "./Cart";
 
 
 function Shop(){
 
     const [goods, setGoods] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [order, setOrder] = useState([]);
 
     useEffect(function getGoods(){
         fetch(API_URL, {
@@ -24,6 +26,7 @@ function Shop(){
     }, []);
 
     return <main className='container content'>
+        <Cart quantity={order.length} />
         {loading ? <Preloader /> : <GoodsList goods={goods} />}
     </main>
 }
